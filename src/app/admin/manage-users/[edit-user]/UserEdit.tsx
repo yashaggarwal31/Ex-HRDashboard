72015c53d3c351c287ee683cb71a19d072df27ab
Yash Aggarwal <yaggarwal@EX2INDIA.LOCAL>
2024-05-29 15:49:31 +0530

    s

--- src/app/admin/manage-users/[edit-user]/UserEdit.tsx
+++ src/app/admin/manage-users/[edit-user]/UserEdit.tsx
@@ -189,7 +189,7 @@ export default function UserEdit({ user, userFields }) {
               className="flex-1 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
               // onClick={() => onCloseView(false)}
             >
-              Cancle
+              Cancel
             </button>
           </Link>
         </div>
